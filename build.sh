9e3186b9fb7d0535177e88963e7bf7d6a0fd539f
Richard Slater <git@richard-slater.co.uk>
2024-10-13 21:00:48 +0100

    build(): add build script

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+if [ "$EUID" -ne 0 ]
+  then echo "use sudo and retry"
+  exit
+fi
+
+source .env
+
+if [[ -z "${USER_HASHED_PASSWORD}" ]]; then
+    echo "USER_HASHED_PASSWORD not in environment, set it and retry"
+    exit
+fi
+
+cp ./configuration.nix /etc/nixos/configuration.nix
+cp ./${HOST}/hardware-configuration.nix /etc/nixos/configuration.nix
+
+sudo nixos-rebuild switch
