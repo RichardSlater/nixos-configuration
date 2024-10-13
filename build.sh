7b1335c81c646ff2a40e41217ba21de73c0a31a8
Richard Slater <git@richard-slater.co.uk>
2024-10-13 21:29:15 +0100

    fix(): environment substitution

--- build.sh
+++ build.sh
@@ -5,14 +5,17 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
-source .env
+source ./.env
 
 if [[ -z "${USER_HASHED_PASSWORD}" ]]; then
     echo "USER_HASHED_PASSWORD not in environment, set it and retry"
     exit
 fi
 
-cp ./configuration.nix /etc/nixos/configuration.nix
+export USER_HASHED_PASSWORD
+
+cat ./configuration.nix | envsubst > ./configuration_merged.nix
+cp ./configuration_merged.nix /etc/nixos/configuration.nix
 cp ./${HOST}/hardware-configuration.nix /etc/nixos/configuration.nix
 
 sudo nixos-rebuild switch
