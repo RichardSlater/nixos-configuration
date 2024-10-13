fe24d3013afaa15d1700de209c90db9681082c51
Richard Slater <git@richard-slater.co.uk>
2024-10-13 21:40:56 +0100

    fix(): substitute only required files

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
+echo "Reading environment."
 source ./.env
 
 if [[ -z "${USER_HASHED_PASSWORD}" ]]; then
@@ -13,8 +14,12 @@ if [[ -z "${USER_HASHED_PASSWORD}" ]]; then
 fi
 
 export USER_HASHED_PASSWORD
+export HOST
 
-cat ./configuration.nix | envsubst > ./configuration_merged.nix
+echo "Substituting variables"
+cat ./configuration.nix | envsubst "$USER_HASHED_PASSWORD" > ./configuration_merged.nix
+diff configuration_merged.nix configuration.nix
+
 cp ./configuration_merged.nix /etc/nixos/configuration.nix
 cp ./${HOST}/hardware-configuration.nix /etc/nixos/configuration.nix
 
